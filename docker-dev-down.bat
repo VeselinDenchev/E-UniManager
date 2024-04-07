aef96e93df7b00cfa188693702d422ea8b7f3437
Veselin Denchev <den4ev01@gmail.com>
2024-04-07 19:50:51 +0300

    Update docker flies and comment the client part

--- docker-dev-down.bat
+++ docker-dev-down.bat
@@ -1,2 +1,2 @@
 @echo off
-docker compose -p dotnet_with_react_separate_development -f docker-compose.yml -f docker-compose.Development.yml --env-file Development.env down
+docker compose -p e_uni_manager_development -f docker-compose.yml -f docker-compose.Development.yml --env-file Development.env down
--- docker-dev-up.bat
+++ docker-dev-up.bat
@@ -3,11 +3,11 @@
 if "%1%" EQU "--build" (goto build) else (goto restart)
 
 :build
-docker compose -p dotnet_with_react_separate_development -f docker-compose.yml -f docker-compose.Development.yml --env-file Development.env up --build
+docker compose -p e_uni_manager_development -f docker-compose.yml -f docker-compose.Development.yml --env-file Development.env up --build
 
 goto end
 
 :restart
-docker compose -p dotnet_with_react_separate_development -f docker-compose.yml -f docker-compose.Development.yml --env-file Development.env up
+docker compose -p e_uni_manager_development -f docker-compose.yml -f docker-compose.Development.yml --env-file Development.env up
 
 :end
