54908fca5fbff04dbaae0b70f5dad1879bc674c7
Veselin Denchev <den4ev01@gmail.com>
2024-05-01 21:15:17 +0300

    Rename _teacherMapper to _mapper

--- EUniManager.Server/src/Core/EUniManager.Application/Services/TeacherService.cs
+++ EUniManager.Server/src/Core/EUniManager.Application/Services/TeacherService.cs
@@ -12,7 +12,7 @@ namespace EUniManager.Application.Services;
 
 public sealed class TeacherService : BaseService<Teacher, Guid, TeacherDto, TeacherDetailsDto>, ITeacherService
 {
-    private readonly TeacherMapper _teacherMapper = new();
+    private readonly TeacherMapper _mapper = new();
     private readonly UserManager<IdentityUser<Guid>> _userManager;
 
     public TeacherService(EUniManagerDbContext dbContext, UserManager<IdentityUser<Guid>> userManager)
@@ -25,7 +25,7 @@ public sealed class TeacherService : BaseService<Teacher, Guid, TeacherDto, Teac
     public override async Task<List<TeacherDto>> GetAllAsync(CancellationToken cancellationToken)
     {
         List<Teacher> teacherEntities = await GetAllEntitiesAsync(cancellationToken);
-        List<TeacherDto> teacherDtos = _teacherMapper.Map(teacherEntities);
+        List<TeacherDto> teacherDtos = _mapper.Map(teacherEntities);
 
         return teacherDtos;
     }
@@ -35,14 +35,14 @@ public sealed class TeacherService : BaseService<Teacher, Guid, TeacherDto, Teac
         Teacher? teacherEntity = await GetEntityByIdAsync(id, cancellationToken);
         ArgumentNullException.ThrowIfNull(teacherEntity);
 
-        TeacherDetailsDto teacherDto = _teacherMapper.Map(teacherEntity);
+        TeacherDetailsDto teacherDto = _mapper.Map(teacherEntity);
 
         return teacherDto;
     }
 
     public override async Task CreateAsync(ICreateDto dto, CancellationToken cancellationToken)
     {
-        Teacher teacher = _teacherMapper.Map((dto as CreateTeacherDto)!);
+        Teacher teacher = _mapper.Map((dto as CreateTeacherDto)!);
         
         var admin = await _userManager.FindByNameAsync("admin@email.com");
         teacher.User = admin;
@@ -52,7 +52,7 @@ public sealed class TeacherService : BaseService<Teacher, Guid, TeacherDto, Teac
     
     public override async Task UpdateAsync(Guid id, IUpdateDto dto, CancellationToken cancellationToken)
     {
-        Teacher teacher = _teacherMapper.Map((dto as UpdateTeacherDto)!);
+        Teacher teacher = _mapper.Map((dto as UpdateTeacherDto)!);
         teacher.Id = id;
         
         var admin = await _userManager.FindByNameAsync("admin@email.com");
