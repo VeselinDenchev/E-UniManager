794701d8dfa2d915fc744352ae9dc44327264b75
Veselin Denchev <den4ev01@gmail.com>
2024-05-03 12:54:40 +0300

    Add Title, StartDate and DueDate to AssignmentConfiguration

--- EUniManager.Server/src/Infrastructure/EUniManager.Persistence/Configurations/EntityTypes/AssignmentConfiguration.cs
+++ EUniManager.Server/src/Infrastructure/EUniManager.Persistence/Configurations/EntityTypes/AssignmentConfiguration.cs
@@ -1,9 +1,11 @@
-﻿using EUniManager.Domain.Entities;
+﻿using EUniManager.Domain.Abstraction.Base.Interfaces;
+using EUniManager.Domain.Entities;
 using EUniManager.Persistence.Configurations.EntityTypes.Base;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
+using static EUniManager.Persistence.Constants.SqlConstant;
 using static EUniManager.Persistence.Constants.Entities.Assignments.AssignmentConstant;
 
 namespace EUniManager.Persistence.Configurations.EntityTypes;
@@ -16,10 +18,18 @@ public sealed class AssignmentConfiguration : BaseEntityConfiguration<Assignment
 
         entity.HasOne(a => a.Resource).WithMany(r => r.Assignments);
 
+        entity.Property(a => a.Title).IsRequired()
+                                     .IsUnicode()
+                                     .HasMaxLength(TITLE_MAX_STRING_LENGTH);
+
+        entity.Property(a => a.StartDate).IsRequired();
+        
+        entity.Property(a => a.DueDate).IsRequired();
+
         entity.Property(а => а.Type).IsRequired()
-                                        .IsUnicode(false)
-                                        .HasConversion<string>()
-                                        .HasMaxLength(TYPE_MAX_STRING_LENGTH);
+                                    .IsUnicode(false)
+                                    .HasConversion<string>()
+                                    .HasMaxLength(TYPE_MAX_STRING_LENGTH);
         
         entity.HasMany(a => a.Students).WithMany(s => s.Assignments);
 
@@ -31,5 +41,20 @@ public sealed class AssignmentConfiguration : BaseEntityConfiguration<Assignment
 
         entity.HasOne(a => a.Teacher).WithMany(t => t.Assignments)
               .OnDelete(DeleteBehavior.NoAction);
+        
+        entity.ToTable(BuildCheckConstraints);
+    }
+    
+    private void BuildCheckConstraints(TableBuilder<Assignment> assignmentsTable)
+    {
+        string[] checkConstraintTokens = [nameof(Assignment), nameof(Assignment.StartDate)];
+        string checkConstraintTableColumn = string.Join('_', checkConstraintTokens);
+        assignmentsTable.HasCheckConstraint(string.Format(CHECK_CONSTRAINT_TEMPLATE, checkConstraintTableColumn), 
+            $"{nameof(Assignment.StartDate)} >= {nameof(IAuditable.CreatedAt)}");
+
+        checkConstraintTableColumn =
+            checkConstraintTableColumn.Replace(nameof(Assignment.StartDate), nameof(Assignment.DueDate));
+        assignmentsTable.HasCheckConstraint(string.Format(CHECK_CONSTRAINT_TEMPLATE, checkConstraintTableColumn), 
+            $"{nameof(Assignment.DueDate)} > {nameof(Assignment.StartDate)}");
     }
 }
--- EUniManager.Server/src/Infrastructure/EUniManager.Persistence/Constants/Entities/Assignments/AssingmentConstant.cs
+++ EUniManager.Server/src/Infrastructure/EUniManager.Persistence/Constants/Entities/Assignments/AssingmentConstant.cs
@@ -2,6 +2,8 @@
 
 public static class AssignmentConstant
 {
+    public const byte TITLE_MAX_STRING_LENGTH = 30;
+    
     public const byte TYPE_MAX_STRING_LENGTH = 10;
     
     public const byte DESCRIPTION_MAX_STRING_LENGTH = 200;
