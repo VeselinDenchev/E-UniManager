b1970f99cb34f4be874e3e1e322f9aa7f8f58f87
Veselin Denchev <den4ev01@gmail.com>
2024-05-03 12:53:09 +0300

    Add Title, StartDate and DueDate to Assignment

--- EUniManager.Server/src/Infrastructure/EUniManager.Persistence/Migrations/20240503094406_Add_Title_StartDate_and_DueDate_to_Assignment.Designer.cs
+++ EUniManager.Server/src/Infrastructure/EUniManager.Persistence/Migrations/20240503094406_Add_Title_StartDate_and_DueDate_to_Assignment.Designer.cs
@@ -4,6 +4,7 @@ using EUniManager.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EUniManager.Persistence.Migrations
 {
     [DbContext(typeof(EUniManagerDbContext))]
-    partial class EUniManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240503094406_Add_Title_StartDate_and_DueDate_to_Assignment")]
+    partial class Add_Title_StartDate_and_DueDate_to_Assignment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EUniManager.Server/src/Infrastructure/EUniManager.Persistence/Migrations/20240503094406_Add_Title_StartDate_and_DueDate_to_Assignment.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EUniManager.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Title_StartDate_and_DueDate_to_Assignment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueDate",
+                table: "Assignments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Assignments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Assignments",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Assignment_DueDate",
+                table: "Assignments",
+                sql: "DueDate > StartDate");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Assignment_StartDate",
+                table: "Assignments",
+                sql: "StartDate >= CreatedAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Assignment_DueDate",
+                table: "Assignments");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Assignment_StartDate",
+                table: "Assignments");
+
+            migrationBuilder.DropColumn(
+                name: "DueDate",
+                table: "Assignments");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Assignments");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Assignments");
+        }
+    }
+}
