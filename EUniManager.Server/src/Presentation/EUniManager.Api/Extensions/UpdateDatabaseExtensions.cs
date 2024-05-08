452e7eae407477b0c88e6a21be641355118fbc9b
Veselin Denchev <den4ev01@gmail.com>
2024-05-08 13:37:22 +0300

    Implement retries in UpdateDatabaseAsync method

--- EUniManager.Server/src/Presentation/EUniManager.Api/Extensions/UpdateDatabaseExtensions.cs
+++ EUniManager.Server/src/Presentation/EUniManager.Api/Extensions/UpdateDatabaseExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using EUniManager.Persistence;
 using EUniManager.Persistence.Seed;
 
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace EUniManager.Api.Extensions;
@@ -9,12 +10,33 @@ public static class UpdateDatabaseExtensions
 {
     public static async Task UpdateDatabaseAsync(this IApplicationBuilder app)
     {
+        const byte MAX_RETRIES_COUNT = 3;
+        
         using IServiceScope serviceScope = app.ApplicationServices.CreateScope();
 
         await using var dbContext = serviceScope.ServiceProvider.GetRequiredService<EUniManagerDbContext>();
-        
-        await dbContext.Database.MigrateAsync();
 
+        byte currentRetriesCount = 0;
+        while (true)
+        {
+            try
+            {
+                await dbContext.Database.ExecuteSqlRawAsync("SELECT 1");
+                
+                break;
+            }
+            catch (SqlException) when (++currentRetriesCount > MAX_RETRIES_COUNT)
+            {
+                throw;
+            }
+            catch (SqlException)
+            {
+                await Task.Delay(10_000);
+                Console.WriteLine($"Could not connect to SQL Server. Retrying {currentRetriesCount}/{MAX_RETRIES_COUNT}.");
+            }
+        }
+
+        await dbContext.Database.MigrateAsync();
         await DatabaseSeeder.SeedAsync(serviceScope.ServiceProvider);
     }
 }
