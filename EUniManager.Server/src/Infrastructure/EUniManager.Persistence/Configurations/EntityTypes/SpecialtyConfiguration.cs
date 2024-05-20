3410333f4e7869e9d2ac0f78d549a23a5684f446
Veselin Denchev <den4ev01@gmail.com>
2024-05-20 16:03:52 +0300

    Update SpecialtyConfiguration

--- EUniManager.Server/src/Infrastructure/EUniManager.Persistence/Configurations/EntityTypes/SpecialtyConfiguration.cs
+++ EUniManager.Server/src/Infrastructure/EUniManager.Persistence/Configurations/EntityTypes/SpecialtyConfiguration.cs
@@ -1,9 +1,11 @@
-﻿using EUniManager.Domain.Entities;
+﻿using EUniManager.Domain.Abstraction.Base.Interfaces;
+using EUniManager.Domain.Entities;
 using EUniManager.Persistence.Configurations.EntityTypes.Base;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
+using static EUniManager.Persistence.Constants.SqlConstant;
 using static EUniManager.Persistence.Constants.Entities.SpecialtyConstant;
 
 namespace EUniManager.Persistence.Configurations.EntityTypes;
@@ -21,11 +23,27 @@ public sealed class SpecialtyConfiguration : BaseEntityConfiguration<Specialty, 
                                     .IsUnicode()
                                     .HasMaxLength(NAME_MAX_STRING_LENGTH);
 
+        entity.Property(s => s.FirstAcademicYearStart).IsRequired();
+        
+        entity.Property(s => s.CurrentYear).IsRequired().HasDefaultValue(CURRENT_YEAR_DEFAULT_VALUE);
+        
+        entity.Property(s => s.Name).IsRequired()
+              .IsUnicode()
+              .HasMaxLength(NAME_MAX_STRING_LENGTH);
+
         entity.HasMany(sp => sp.Students).WithOne(st => st.Specialty)
               .OnDelete(DeleteBehavior.NoAction)
               .IsRequired(false);
 
         entity.HasMany(sp => sp.Subjects).WithOne(sub => sub.Specialty)
               .IsRequired();
+
+        entity.ToTable(table =>
+        {
+              string[] checkConstraintTokens = [nameof(Specialty), nameof(Specialty.FirstAcademicYearStart)];
+              string checkConstraintTableColumn = string.Join('_', checkConstraintTokens);
+              table.HasCheckConstraint(string.Format(CHECK_CONSTRAINT_TEMPLATE, checkConstraintTableColumn), 
+                    $"{nameof(Specialty.FirstAcademicYearStart)} BETWEEN {DateTime.Now.Year} AND {MAX_FIRST_ACADEMIC_YEAR_START}");
+        });
     }
 }
--- EUniManager.Server/src/Infrastructure/EUniManager.Persistence/Constants/Entities/SpecialtyConstant.cs
+++ EUniManager.Server/src/Infrastructure/EUniManager.Persistence/Constants/Entities/SpecialtyConstant.cs
@@ -3,4 +3,8 @@
 public class SpecialtyConstant
 {
     public const byte NAME_MAX_STRING_LENGTH = 100;
+
+    public const short MAX_FIRST_ACADEMIC_YEAR_START = 2099;
+    
+    public const short CURRENT_YEAR_DEFAULT_VALUE = 1;
 }
