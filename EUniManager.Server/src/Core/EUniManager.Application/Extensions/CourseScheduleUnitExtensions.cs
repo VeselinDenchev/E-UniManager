90fdb06e0b9672e92190a0c68d314b6959ea1b22
Veselin Denchev <den4ev01@gmail.com>
2024-06-05 22:48:55 +0300

    Remove commented code

--- EUniManager.Server/src/Core/EUniManager.Application/Extensions/CourseScheduleUnitExtensions.cs
+++ EUniManager.Server/src/Core/EUniManager.Application/Extensions/CourseScheduleUnitExtensions.cs
@@ -4,13 +4,6 @@ namespace EUniManager.Application.Extensions;
 
 public static class CourseScheduleUnitExtensions
 {
-    // public static IQueryable<CourseScheduleUnit> OrderSchedule(this IQueryable<CourseScheduleUnit> courseScheduleUnits)
-    // {
-    //     return courseScheduleUnits.OrderBy(csu => csu.DayOfWeek).ThenBy(csu => csu.StartTime)
-    //                                                             .ThenBy(csu => csu.GroupType)
-    //                                                             .ThenBy(csu => csu.GroupNumber);
-    // }
-    
     public static List<CourseScheduleUnit> ToScheduleOrderedList(this IEnumerable<CourseScheduleUnit> courseScheduleUnits)
     {
         return courseScheduleUnits.OrderBy(csu => csu.DayOfWeek).ThenBy(csu => csu.StartTime)
