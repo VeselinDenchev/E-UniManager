3541da736c94483678ddb6da39a885f6f29ccd9d
Veselin Denchev <den4ev01@gmail.com>
2024-08-11 18:52:07 +0300

    Set API port dynamically in Vite config

--- EUniManager.Client/vite.config.js
+++ EUniManager.Client/vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv  } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    strictPort: true,
-    port: 3000
-  }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  const apiUrl = new URL(env.VITE_CLIENT_BASE_URL);
+
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      strictPort: true,
+      port: apiUrl.port
+    }
+  };
 })
