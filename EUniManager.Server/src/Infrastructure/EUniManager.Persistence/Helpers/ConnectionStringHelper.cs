24a70950e6eb4d2efd2009a70cb8708c64716b0b
Veselin Denchev <den4ev01@gmail.com>
2024-08-23 22:31:48 +0300

    Throw exception properly when data for connection string is null

--- EUniManager.Server/src/Infrastructure/EUniManager.Persistence/Helpers/ConnectionStringHelper.cs
+++ EUniManager.Server/src/Infrastructure/EUniManager.Persistence/Helpers/ConnectionStringHelper.cs
@@ -12,12 +12,12 @@ public static class ConnectionStringHelper
         string canNotLoadEnvVariableMessage =
             string.Format(CAN_NOT_LOAD_ENV_VARIABLE_MESSAGE_TEMPLATE, DATABASE_NAME_ENV_VARIABLE_NAME);
         string databaseName = Environment.GetEnvironmentVariable(DATABASE_NAME_ENV_VARIABLE_NAME) ?? 
-                              throw new ArgumentNullException(canNotLoadEnvVariableMessage);
+                              throw new ArgumentException(canNotLoadEnvVariableMessage);
 
         canNotLoadEnvVariableMessage =
             canNotLoadEnvVariableMessage.Replace(DATABASE_NAME_ENV_VARIABLE_NAME, SA_PASSWORD_ENV_VARIABLE_NAME);
         string saPassword = Environment.GetEnvironmentVariable(SA_PASSWORD_ENV_VARIABLE_NAME) ?? 
-                            throw new ArgumentNullException(canNotLoadEnvVariableMessage);
+                            throw new ArgumentException(canNotLoadEnvVariableMessage);
         
         string connectionString = $"Data Source=sqlserver;Initial Catalog={databaseName};TrustServerCertificate=True;User ID=SA;Password={saPassword}";
 
